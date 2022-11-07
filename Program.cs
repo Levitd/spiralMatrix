7d11717bbfab6252542da721b04f0449c74a81fc
LevitD <diml71@mail.ru>
2022-11-07 21:50:33 +0300

    Spiral Matrix

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.RegularExpressions;
+using System;
+using static System.Console;
+using System.Data;
+
+Clear();
+try
+{
+    Write("Enter matrix size: ");
+    string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    int[,] array = GetArray(int.Parse(nums[0]));
+
+    PrintArray(array);
+
+}
+catch
+{
+    Write("Error run!");
+}
+
+int[,] GetArray(int rows)
+{
+    int[,] result = new int[rows, rows];
+    int i = 0, j = 0;
+    int number = 1;
+    
+    int minX = 0;
+    int minY = 0;
+    int maxX = result.GetLength(0);
+    int maxY = result.GetLength(1);
+    while (minX < maxX)
+    {
+        while (j < maxY)
+        {
+            result[i, j] = number;
+            number++;
+            j++;
+        }
+        j--;
+        i++;
+        while (i < maxX)
+        {
+            result[i, j] = number;
+            number++;
+            i++;
+        }
+        i--;
+        j--;
+        while (j >= minY)
+        {
+            result[i, j] = number;
+            number++;
+            j--;
+        }
+        i--;
+        j++;
+        while (i > minY)
+        {
+            result[i, j] = number;
+            number++;
+            i--;
+        }
+        i++;
+        j++;
+        minX++;
+        minY++;
+        maxX--;
+        maxY--;
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j].ToString().PadLeft(5, ' ')} ");
+        }
+        WriteLine();
+    }
+}
+
